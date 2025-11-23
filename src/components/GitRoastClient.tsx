a00fe26cf26f3a948fc09a053f376850ee842ece
Md Kasif Uddin <mdkasifuddin121@gmail.com>
2025-11-23 09:19:38 +0000

    In the home page show the GitHub icon clearly in input filed

--- src/components/GitRoastClient.tsx
+++ src/components/GitRoastClient.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useActionState, useEffect, useState } from 'react';
@@ -55,7 +56,7 @@ export default function GitRoastClient() {
     <section className="w-full max-w-md md:max-w-xl lg:max-w-4xl">
         <form action={handleFormAction} className="relative flex flex-col sm:flex-row items-center gap-4">
             <div className="relative w-full">
-                <Github className="absolute left-4 top-1/2 -translate-y-1/2 text-muted-foreground" />
+                <Github className="absolute left-4 top-1/2 -translate-y-1/2 text-primary w-6 h-6" />
                 <Input
                     type="text"
                     name="username"
