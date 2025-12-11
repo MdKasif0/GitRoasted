9173b818f577da3f43560778a38bd486957488c7
Md Kasif Uddin <161304860+MdKasif0@users.noreply.github.com>
2025-12-11 13:56:47 +0530

    Fix API key casing in Firebase config
    Updated API key to use the correct casing.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "Gemini_API_key",
+  apiKey: "GEMINI_API_KEY",
   authDomain: "gitroasted.firebaseapp.com",
   projectId: "gitroasted",
   storageBucket: "gitroasted.appspot.com",
